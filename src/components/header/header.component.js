0475aa0a9957a203d98dda478fcb4ff99b21d78e
Raul Soledispa <soledispa.raul@gmail.com>
2021-12-20 10:31:29 -0500

    cart-item added

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -5,7 +5,7 @@ import { ReactComponent as Logo} from "../../assets/crown.svg";
 import {auth} from "../../firebase/firebase.util";
 import {connect} from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component";
-import {CartDropdown} from "../cart-dropdown/cart-dropdown.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 
 const Header = ({ currentUser, hidden}) => (
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import {CartTypes} from "./cart.types";
+import {addItemToCart} from "./cart.uitl";
 
 const INITIAL_STATE =  {
   hidden: false,
@@ -13,7 +14,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     }
     case CartTypes.ADD_ITEM: return {
       ...state,
-      cartItems: [...state.cartItems, action.payload]
+      cartItems: addItemToCart(state.cartItems, action.payload)
     }
 
     default: return state;
